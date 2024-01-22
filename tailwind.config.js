412042c09f09c51fa46ce4c53ed2b1125acf6c8a
malikrehman18 <uanees678@gmail.com>
2024-01-22 16:39:04 +0500

    Hero & Feature section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Josefin Sans", "sans-serif"],
+        alata: ["Alata"],
+      },
+    },
   },
   plugins: [],
 };
